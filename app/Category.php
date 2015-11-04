9affab5f0867e266da30aa64d2f63027af12aeab
Enrique Sampaio dos Santos <enrique@pilhadigital.com.br>
2015-11-04 11:11:26 -0200

    Criando relações nos models

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Keyword;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     public $timestamps = false;
+
+    public function keywords()
+    {
+        return $this->hasMany('Keyword');
+    }
 }
