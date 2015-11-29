4cfbc742210f1486e3b6c1f325201a0bffb17aa6
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2015-11-29 01:09:54 -0200

    Parser para todos meses em uma planilha basicamente pronto, precisa criar variável fora do foreach para atribuir o resultado e poder usar fora no save

--- app/Http/Controllers/InvestimentController.php
+++ app/Http/Controllers/InvestimentController.php
@@ -8,6 +8,7 @@ use App\Category;
 use App\City;
 use App\Investiment;
 use App\Keyword;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -45,6 +46,9 @@ class InvestimentController extends Controller
             'year' => 'required',
         ]);
 
+        $city = City::findOrFail($request->city);
+        $investiments = array();
+        $investimentIdx = 0;
         $tags = Keyword::get();
         $parser = PHPExcel_IOFactory::load($request->file);
         $columns = array_fill(1, PHPExcel_Cell::columnIndexFromString($parser->getActiveSheet()->getHighestColumn()), 0);
@@ -60,19 +64,55 @@ class InvestimentController extends Controller
         if (empty($request->month)) {
             foreach ($parser->getActiveSheet()->getRowIterator() as $row) {
                 foreach ($row->getCellIterator() as $cell) {
+                    $investiments[$investimentIdx] = array();
                     $value = PHPExcel_Cell::columnIndexFromString($cell->getColumn());
                     if ($value == array_keys($columns, max($columns))[0]) {
+                        $flag = TRUE;
                         foreach ($tags as $tag) {
                             if (strpos(strtolower($cell->getValue()), $tag->name) !== FALSE) {
-                                //match match pow pow
+                                $flag = FALSE;
+                                $category = Category::findOrFail($tag->category_id);
+                                for ($i=1; $i <= 12; $i++) {
+                                    $investiments[$investimentIdx]['column'] = $cell->getColumn();
+                                    $investiments[$investimentIdx]['row'] = $cell->getRow();
+                                    $investiments[$investimentIdx][$i] = new Investiment;
+                                    $investiments[$investimentIdx][$i]->category()->associate($category);
+                                    $investiments[$investimentIdx][$i]->city()->associate($city);
+                                    $investiments[$investimentIdx][$i]->name = $cell->getValue();
+                                }
+                                break;
                             }
                         }
+                        if ($flag) {
+                            $category = Category::where('name', 'Outros')->firstOrFail();
+                            for ($i=1; $i <= 12; $i++) {
+                                $investiments[$investimentIdx]['column'] = $cell->getColumn();
+                                $investiments[$investimentIdx]['row'] = $cell->getRow();
+                                $investiments[$investimentIdx][$i] = new Investiment;
+                                $investiments[$investimentIdx][$i]->category()->associate($category);
+                                $investiments[$investimentIdx][$i]->city()->associate($city);
+                                $investiments[$investimentIdx][$i]->name = $cell->getValue();
+                            }
+                        }
+                    } else if (isset($investiments[$investimentIdx]['row']) && $cell->getRow() === $investiments[$investimentIdx]['row'] && $month = ord($cell->getColumn()) - ord($investiments[$investimentIdx]['column']) >= 1 && $month <= 12) {
+                        $investiments[$investimentIdx][$month]->value = $cell->getValue();
+                        $investiments[$investimentIdx][$month]->made_at = $request->year.'-'.$month;
                     }
+                }
+                $investimentIdx++;
+            }
+
+            foreach ($investiments as $investiment) {
+                foreach ($investiment as $singleMonth) {
+                    echo $singleMonth->name.' '.$singleMonth->value.' '.$singleMonth->date.'<br>';
+                    $singleMonth->save();
                 }
             }
         } else {
             # code...
         }
+
+        // return Redirect::route('admin.investimentos.index');
     }
 
     /**
